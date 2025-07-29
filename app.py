e6a66c8a9e02f430e1d88cbcf4b78af8cae7298e
JacoboT27 <jacobotc2703@gmail.com>
2025-07-29 16:14:34 -0700

    More functions

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import yfinance as yf
 import numpy as np
-from sklearn.metrics import r2_score
-from functions import compute_rsi, getDividendInfo, getStockInfo
+from functions import compute_rsi, getDividendInfo, getStockInfo, linearRegression, getVixData, getSMA, SMATrend, getPriceMXN
 
 app = Flask(__name__)
 
@@ -13,126 +12,64 @@ def index():
 @app.route('/api/stock/<ticker>')
 def stock_api(ticker):
 
-    period = request.args.get('period', '1y') #1 year is default period
-    stock, beta, hist = getStockInfo(ticker, period)
-    dividendRate, ex_div_date, dividendTail, payoutratio = getDividendInfo(stock)
+    # --- Basic Info ---
+    period = request.args.get('period', '1y')                                           # 1 year is default period
+    stock, beta, hist = getStockInfo(ticker, period)                                    # Stock information
+    dividendRate, ex_div_date, dividendTail, payoutratio = getDividendInfo(stock)       # Dividend information
 
-    close = hist['Close'].ffill()
-    ln_close = np.log(close)
+    # --- Close Data (CHART 1) ---
+    close = hist['Close'].ffill()                                                       # Close data
 
-    # Linear regression (y = mx + b)
-    x = np.arange(len(ln_close))
-    m, b = np.polyfit(x, ln_close, 1)
-    regression = m * x + b
-    r2 = r2_score(ln_close, regression)
+    # --- Ln Data (CHART 2) ---
+    ln_close = np.log(close)                                                            # Ln of Close data
+    x, m, b, regression, r2 = linearRegression(ln_close)                                # Linear regression (y = mx + b)
 
-    #2mo for rsi
-    rsi_hist = stock.history(period='4mo')
-    rsi_close = rsi_hist['Close'].ffill()
-    # Compute RSI
-    rsi = compute_rsi(rsi_close)
-    # Filter out NaN values for RSI
-    rsi = rsi.dropna()
-    # last 30 values of RSI
-    rsi = rsi.tail(30)
-    rsi_dates = rsi.index.strftime('%m-%d').tolist()
+    # --- RSI Data (CHART 3) --- 
+    rsi_hist = stock.history(period='4mo')                                              # RSI Data
+    rsi_close = rsi_hist['Close'].ffill()                                               # RSI Close
+    rsi = compute_rsi(rsi_close)                                                        # Compute RSI
+    rsi = rsi.dropna()                                                                  # Drop NaN
+    rsi = rsi.tail(30)                                                                  # Last 30 values of RSI
+    rsi_dates = rsi.index.strftime('%m-%d').tolist()                                    # Index Dates
 
-    #VIX 3months
-    vix = yf.Ticker('^VIX')
-    vix_hist = vix.history(period='5y')
-    vix_close = vix_hist['Close'].ffill()
-    vix_close_full = vix_close.dropna()
-    vix_close = vix_close_full.tail(30)  # Last 30 days
-    vix_dates = vix_close.index.strftime('%Y-%m-%d').tolist()
-    #VIX sma5
-    vix_sma5 = vix_close.rolling(window=5).mean()
-    vix_sma5 = vix_sma5.dropna()
-    vix_dates = vix_sma5.index.strftime('%m-%d').tolist()
-    recent_vix = vix.info.get('regularMarketPrice', 0)
+    # --- SMA Data (CHART 4) --- 
+    sma200, close_sma = getSMA(stock,200)                                               # SMA200 Data
+    sma200 = sma200.tail(90)                                                            # Limit to 90 values
+    sma20, close_sma = getSMA(stock,20)                                                 # SMA20 Data
+    aligned_dates = sma200.index                                                        # Aligned Dates Inedx
+    sma20_aligned = sma20.loc[aligned_dates]                                            # Align SMA20 Dates
+    close_sma_aligned = close_sma.loc[aligned_dates]                                    # Align Close Data
+    price_trend = SMATrend(close, sma20_aligned, sma200)                                # Price Trend Based on SMA
 
-    if (vix_close.iloc[-1] < 15):
-        vix_state = "Low VIX - Market Calm ✅"
-    elif (vix_close.iloc[-1] < 20):
-        vix_state = "Moderate VIX - Market Stable ✅"
-    elif (vix_close.iloc[-1] < 30):
-        vix_state = "High VIX - Market Volatile ⚠️"
-    else:
-        vix_state = "Extreme VIX - Market Panic ❌"
+    # --- VIX Data (CHART 5) ---    
+    vix_close, vix_sma5, vix_dates, vix_state, vix_trend, recent_vix, vix_close_full = getVixData() # VIX Data
 
-    if (vix_close.iloc[-1] >= vix_sma5.iloc[-1]):
-        vix_trend = 'VIX Rising - Go to Safe Sectors ⚠️'
-    else:
-        vix_trend = 'VIX Falling - Risk On ✅'
-
-    #Stock sma200
-    sma200_hist = stock.history(period='5y')
-    sma200_close = sma200_hist['Close'].ffill()
-    sma200 = sma200_close.rolling(window=200).mean().dropna().tail(90)  # Last 3 months
-    #Stock sma20
-    sma20 = sma200_close.rolling(window=20).mean().dropna()
-    #Stock close
-    close_sma = sma200_hist['Close'].ffill().dropna()
-    # Align all series based on SMA200's index
-    aligned_dates = sma200.index
-    sma20_aligned = sma20.loc[aligned_dates]
-    close_sma_aligned = close_sma.loc[aligned_dates]
+    # --- XLY/XLP Ratio (CHART 6) ---
+    xlp = yf.Ticker('XLP')                                                              # XLP Ticker
+    xlp_hist = xlp.history(period='100d')                                               # XLP History
+    xlp_close = xlp_hist['Close'].ffill().dropna()                                      # XLP Close
+    xly = yf.Ticker('XLY')                                                              # XLY Ticker
+    xly_hist = xly.history(period='100d')                                               # XLY History
+    xly_close = xly_hist['Close'].ffill().dropna()                                      # XLY Close
+    common_index = xlp_close.index.intersection(xly_close.index)                        # Common Index
+    xlp_close = xlp_close.loc[common_index]                                             # Align XLP
+    xly_close = xly_close.loc[common_index]                                             # Align XLY
+    ratio = (xly_close / xlp_close).dropna()                                            # Ratio
+    sma10_ratio = ratio.rolling(window=10).mean().dropna()                              # Ratio SMA10 
+    aligned_index = sma10_ratio.index                                                   # Aligned Index
+    ratio = ratio.loc[aligned_index]                                                    # Aligned Ratio
+    ratio_dates = aligned_index.strftime('%m-%d').tolist()                              # Aligned Dates
     
-    if (close.iloc[-1] > sma20_aligned.iloc[-1]) and (close.iloc[-1] > sma200.iloc[-1]):
-        price_trend = " Strong Uptrend ✅"
-    elif (close.iloc[-1] > sma20_aligned.iloc[-1]) and (sma20_aligned.iloc[-1] < sma200.iloc[-1]):
-        price_trend = "Long-Term Downtrend ❌"
-    elif (close.iloc[-1] < sma20_aligned.iloc[-1]) and (sma20_aligned.iloc[-1] > sma200.iloc[-1]):
-        price_trend = "Good Entry Point ✅"
-    elif (close.iloc[-1] < sma20_aligned.iloc[-1]) and (sma20_aligned.iloc[-1] < sma200.iloc[-1]):
-        price_trend = "Strong Downtrend ❌"
-    elif (sma20_aligned.iloc[-1] > sma200.iloc[-1]) and (close.iloc[-1] < sma20_aligned.iloc[-1]):
-        price_trend = "Short-Term Downtrend ⚠️"
-    elif (sma20_aligned.iloc[-1] < sma200.iloc[-1]) and (close.iloc[-1] > sma20_aligned.iloc[-1]):
-        price_trend = "Short-Term Uptrend ⚠️"
-    else:
-        price_trend = "No Clear Trend ⚠️"
-
-    #calculate CAGR with slope of ln
-    cagr = np.exp(m*252) - 1
-
-    #XLP data
-    xlp = yf.Ticker('XLP')
-    xlp_hist = xlp.history(period='100d')
-    xlp_close = xlp_hist['Close'].ffill().dropna()
-    #XLY data
-    xly = yf.Ticker('XLY')
-    xly_hist = xly.history(period='100d')
-    xly_close = xly_hist['Close'].ffill().dropna()
-    # Align indices
-    common_index = xlp_close.index.intersection(xly_close.index)
-    xlp_close = xlp_close.loc[common_index]
-    xly_close = xly_close.loc[common_index]
-    ratio = xly_close / xlp_close
-    ratio = ratio.dropna()
-    # SMA10 of the ratio
-    sma10_ratio = ratio.rolling(window=10).mean().dropna()
-    # Align dates for plotting
-    aligned_index = sma10_ratio.index  # to keep the same dates for both lines
-    ratio = ratio.loc[aligned_index]   # align original ratio with sma10
-    ratio_dates = aligned_index.strftime('%m-%d').tolist()
-
-    # USD/MXN current price
-    usd_mxn = yf.Ticker('USDMXN=X')
-    mxn_price = usd_mxn.info.get('regularMarketPrice', 0)
-    stock_currency = stock.info.get('currency', 'USD')
-
-    # Current Price for Mexico
-    if stock_currency == 'USD':
-        price = stock.info.get('regularMarketPrice', 0) * mxn_price
-    if stock_currency == 'MXN':
-        price = stock.info.get('regularMarketPrice', 0)
-
-    #correlation between stock and VIX last 200 days
-    sma200_close.index = sma200_close.index.tz_localize(None)
+    # --- Data Section ---
+    cagr = np.exp(m*252) - 1                                                            # CAGR
+    mxn_price, stock_currency, price = getPriceMXN(stock)                               # Exchange and Price in MXN
+    
+    # Correlation
+    close_sma.index = close_sma.index.tz_localize(None)
     vix_close_full.index = vix_close_full.index.tz_localize(None)
-    common_index = sma200_close.index.intersection(vix_close_full.index).dropna()
+    common_index = close_sma.index.intersection(vix_close_full.index).dropna()
     vix_aligned = vix_close_full.loc[common_index]
-    stock_aligned = sma200_close.loc[common_index]
+    stock_aligned = close_sma.loc[common_index]
     # Drop any remaining NaNs (just in case)
     vix_aligned = vix_aligned.dropna()
     stock_aligned = stock_aligned.dropna()
--- functions.py
+++ functions.py
@@ -1,4 +1,26 @@
 import yfinance as yf
+import numpy as np
+from sklearn.metrics import r2_score
+
+def getDividendInfo(stock):
+    dividendRate = stock.info.get('dividendRate', None)
+    ex_div_date = stock.info.get('exDividendDate', None)
+    dividendTail = stock.dividends.tail(5)
+    payoutratio = stock.info.get("payoutRatio", None)
+    return dividendRate, ex_div_date, dividendTail, payoutratio
+
+def getStockInfo(ticker, period):
+    stock = yf.Ticker(ticker)
+    beta = stock.info.get("beta", "N/A")
+    hist = stock.history(period=period)
+    return stock, beta, hist
+
+def linearRegression(ln_close):
+    x = np.arange(len(ln_close))                                                       
+    m, b = np.polyfit(x, ln_close, 1)
+    regression = m * x + b
+    r2 = r2_score(ln_close, regression)
+    return x, m, b, regression, r2
 
 def compute_rsi(series, period=21):
     delta = series.diff()
@@ -12,15 +34,61 @@ def compute_rsi(series, period=21):
     rsi = 100 - (100 / (1 + rs))
     return rsi
 
-def getDividendInfo(stock):
-    dividendRate = stock.info.get('dividendRate', None)
-    ex_div_date = stock.info.get('exDividendDate', None)
-    dividendTail = stock.dividends.tail(5)
-    payoutratio = stock.info.get("payoutRatio", None)
-    return dividendRate, ex_div_date, dividendTail, payoutratio
+def getVixData():
+    vix = yf.Ticker('^VIX')                                                             # VIX ticker
+    vix_hist = vix.history(period='5y')                                                 # VIX history
+    vix_close = vix_hist['Close'].ffill()                                               # VIX Close
+    vix_close_full = vix_close.dropna()                                                 # Drop NaN
+    vix_close = vix_close_full.tail(30)                                                 # Last 30 values of VIX
+    vix_dates = vix_close.index.strftime('%Y-%m-%d').tolist()                           # Index Dates
+    vix_sma5 = vix_close.rolling(window=5).mean()                                       # VIX SMA5 
+    vix_sma5 = vix_sma5.dropna()                                                        # Drop NaN
+    vix_dates = vix_sma5.index.strftime('%m-%d').tolist()                               # Index Dates
+    recent_vix = vix.info.get('regularMarketPrice', 0)                                  # Latest VIX Value
+    if (recent_vix < 15):
+        vix_state = "Low VIX - Market Calm ✅"                                         # VIX State
+    elif (recent_vix < 20):
+        vix_state = "Moderate VIX - Market Stable ✅"
+    elif (recent_vix < 30):
+        vix_state = "High VIX - Market Volatile ⚠️"
+    else:
+        vix_state = "Extreme VIX - Market Panic ❌"
+    if (recent_vix >= vix_sma5.iloc[-1]):
+        vix_trend = 'VIX Rising - Go to Safe Sectors ⚠️'                               # VIX Trend
+    else:
+        vix_trend = 'VIX Falling - Risk On ✅'
 
-def getStockInfo(ticker, period):
-        stock = yf.Ticker(ticker)
-        beta = stock.info.get("beta", "N/A")
-        hist = stock.history(period=period)
-        return stock, beta, hist
+    return vix_close, vix_sma5, vix_dates, vix_state, vix_trend, recent_vix, vix_close_full
+
+def getSMA(stock,window):
+    sma_hist = stock.history(period='5y')
+    close_sma = sma_hist['Close'].ffill().dropna()
+    sma = close_sma.rolling(window).mean().dropna()
+    return sma, close_sma
+
+def SMATrend(close, sma20_aligned, sma200):
+    if (close.iloc[-1] > sma20_aligned.iloc[-1]) and (close.iloc[-1] > sma200.iloc[-1]):
+        price_trend = " Strong Uptrend ✅"
+    elif (close.iloc[-1] > sma20_aligned.iloc[-1]) and (sma20_aligned.iloc[-1] < sma200.iloc[-1]):
+        price_trend = "Long-Term Downtrend ❌"
+    elif (close.iloc[-1] < sma20_aligned.iloc[-1]) and (sma20_aligned.iloc[-1] > sma200.iloc[-1]):
+        price_trend = "Good Entry Point ✅"
+    elif (close.iloc[-1] < sma20_aligned.iloc[-1]) and (sma20_aligned.iloc[-1] < sma200.iloc[-1]):
+        price_trend = "Strong Downtrend ❌"
+    elif (sma20_aligned.iloc[-1] > sma200.iloc[-1]) and (close.iloc[-1] < sma20_aligned.iloc[-1]):
+        price_trend = "Short-Term Downtrend ⚠️"
+    elif (sma20_aligned.iloc[-1] < sma200.iloc[-1]) and (close.iloc[-1] > sma20_aligned.iloc[-1]):
+        price_trend = "Short-Term Uptrend ⚠️"
+    else:
+        price_trend = "No Clear Trend ⚠️"
+    return price_trend
+
+def getPriceMXN(stock):
+    usd_mxn = yf.Ticker('USDMXN=X')                                                     # Exchange Rate
+    mxn_price = usd_mxn.info.get('regularMarketPrice', 0)                               # Current Price of USD
+    stock_currency = stock.info.get('currency', 'USD')                                  # Currency of Stock
+    if stock_currency == 'USD':
+        price = stock.info.get('regularMarketPrice', 0) * mxn_price                     # Price in MXN
+    if stock_currency == 'MXN':
+        price = stock.info.get('regularMarketPrice', 0)
+    return mxn_price, stock_currency, price
